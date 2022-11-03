ac4afef563fcf77375f4e691c6f3e202c518f9c2
IT086469 <IT086469@cerner.net>
2022-11-03 22:23:21 +0200

    create Article mode and  seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if ActiveRecord::Base.connection.table_exists? 'articles'
+  Article.where(title: 'I love Ruby!', category: 'ruby').first_or_create
+  Article.where(title: 'I love Elasticsearch!', category: 'elasicsearch').first_or_create
+  Article.where(title: "Why it's always sunny in California", category: 'other').first_or_create
+end
